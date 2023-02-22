591275b3305401253dc18a23b8bad7d69b8e379f
RyotaroKoshio <kabosulike@icloud.com>
2023-02-22 18:24:50 +0900

    セキュアなパスワードの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+	before_save { self.email = self.email.downcase }
   validates :name,  presence: true , length: { maximum: 50 }#user nameが空となることを許さないようにする。これのおかげでセーブができないようになり、テストコードの"name should be present"などが無視されるようになりGreenになる。
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 end
