4e2318cc0b03b104e0959a1f1cc2f10a70a080ac
RyotaroKoshio <kabosulike@icloud.com>
2023-02-28 15:15:25 +0900

    スタイルシートの変更を忘れていただけだった。

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,9 +1,9 @@
 // メニュー操作
 
 // トグルリスナーを追加してクリックをリッスンする
-document.addEventListener("turbo:load", function() {
+document.addEventListener("turbo:load", function () {
   let account = document.querySelector("#account");
-  account.addEventListener("click", function(event) {
+  account.addEventListener("click", function (event) {
     event.preventDefault();
     let menu = document.querySelector("#dropdown-menu");
     menu.classList.toggle("active");
